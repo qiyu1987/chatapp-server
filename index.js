f8ff58e87771bae92df77bcb55e62f5bf3338c2b
qiyu1987 <qiyu1987@gmail.com>
2019-10-25 11:26:23 +0200

    posting messages

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const streamRouter = require('./stream/router')
+
+const app = express()
+const port = process.env.Port || 5000
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+app.get('/', (req, res) => {
+    console.log("get an request on /")
+    res.status(200)
+    res.send("hello world!")
+})
+
+app.use(streamRouter)
---
+++ stream/router.js
@@ -0,0 +1,24 @@
+const {Router} = require('express')
+const Chatroom = require('./model')
+
+const router = new Router()
+
+
+router.get('/stream', (req, res) => {
+    console.log("got a request on /stream")
+    res.status(200)
+    res.send("it works")
+})
+
+router.post('/message', async (req, res) => {
+    console.log("got a request on /message",req.body)
+    const {message} = req.body
+    const entity = await Chatroom.create({
+        message
+    })
+
+    res.status(201)
+    res.send("Thanks for your message")
+})
+
+module.exports = router
