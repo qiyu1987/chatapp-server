df310058bfe98ec84c58b0ecadc17b253043ebe7
qiyu1987 <qiyu1987@gmail.com>
2019-10-25 11:49:27 +0200

    set stream to work

--- stream/router.js
+++ stream/router.js
@@ -1,13 +1,19 @@
 const {Router} = require('express')
 const Chatroom = require('./model')
+const Sse = require('json-sse')
 
 const router = new Router()
+const stream = new Sse()
 
 
-router.get('/stream', (req, res) => {
+router.get('/stream', async (req, res) => {
     console.log("got a request on /stream")
-    res.status(200)
-    res.send("it works")
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    console.log("messages in db:", data)
+    stream.updateInit(data)
+    stream.init(req, res)
 })
 
 router.post('/message', async (req, res) => {
@@ -16,6 +22,12 @@ router.post('/message', async (req, res) => {
     const entity = await Chatroom.create({
         message
     })
+
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    // console.log("messages in db:", data)
+    stream.send(data)
 
     res.status(201)
     res.send("Thanks for your message")
